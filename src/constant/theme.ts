50bd6b6c258cdad1096f8ac0d1ae76a7971711ac
Yanis Mignot <yayamgt@device-15.home>
2026-04-15 13:28:05 +0200

    fix: error linter theme.ts

--- src/constant/theme.ts
+++ src/constant/theme.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const tailwindConfig = require('@/tailwind.config');
+const tailwindConfig = require('../../tailwind.config.js');
 
-export const colors: any = tailwindConfig.theme.extend.colors;
+export const colors = tailwindConfig.theme.extend.colors;
