55398459de4e56c910a616d487ab174813f75998
Thomas Roche-Rebelle <thomas.roche-rebelle@epitech.eu>
2026-06-05 23:37:56 +0200

    fix: assetPrefix next.config.ts correct variable

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: '/koudmain-web',
-  assetPrefix: '/koudmain-web',
+  basePath: '/Koudmain-web',
+  assetPrefix: '/Koudmain-web/',
 };
 
 export default nextConfig;
